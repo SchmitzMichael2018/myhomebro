3225fd3e665c757a143d4cb2b249a1bae5b99998
Michael Schmitz <schmitzmichael1985@gmail.com>
2026-09-16 14:55:35 -0500

    Merge parallel contractor migration joins

---
+++ backend/projects/migrations/0313_merge_parallel_0312_branches.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("projects", "0312_merge_0294_exclude_prelaunch_and_0311_hide_qa"),
+        ("projects", "0312_merge_referrals_and_contractor_lifecycle"),
+    ]
+
+    operations = []
